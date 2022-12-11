52f55d66db1dacd203f422e9976359006727e10b
TUNBudi06 <70352065+TUNBudi06@users.noreply.github.com>
2022-12-11 11:17:00 +0700

    Update install-build.sh

--- install-build.sh
+++ install-build.sh
@@ -2,13 +2,13 @@ pkg update
 pkg upgrade -y
 pkg install cmake m4 ninja bison \
 php python make build-essential \
-texinfo rust rust-dev llvm shc \
+texinfo rust* rustc-dev llvm shc \
 bison-static gn samurai -y
 
 pkg install autoconf resolv-conf \
 autoconf-archive autoconf213 \
 libconfig libconfig-static \
-libt3config libltdl-static \
+libt3config libltdl \
 libltdl libffi-static libffi \
 libelf-static libelf elfutils \
 libdw-static libdw gettext intltool \
